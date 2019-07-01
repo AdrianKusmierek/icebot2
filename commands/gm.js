e00aabb527cea16735aba06e59dec1aab4874ba5
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-07-02 00:14:15 +0200

    Create gm.js

---
+++ commands/gm.js
@@ -0,0 +1,7 @@
+module.exports = {
+        name: '\gm',
+        description: 'Displays: Good Morning **Nickname & Tag**!! **PhotoURL**',
+        execute(message, args) {
+                
+        },
+};
