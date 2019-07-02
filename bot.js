3e6578fb2fbd6f29cbebc07372dcfcbc49b00691
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-07-02 08:07:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,38 +83,6 @@ client.on("message", async message => {
     if (command === 'ping') {
 	message.channel.send('Pong!');
     }
-  if (command === 'info') {
-		message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "Info",
-    description: "Dit is alles wat u moet weten over de IceTopia Bot.",
-    fields: [{
-        name: "Ontwikkelaar",
-        value: "De trotse ontwikkelaar van de IceTopia Bot is Adrian Kuśmierek."
-      },
-      {
-        name: "Ontwikkeling",
-        value: "De IceTopia Bot is gecreëerd op 25/6/2019 en wordt sindsdien ontwikkeld."
-      },
-      {
-        name: "Website",
-        value: "Als je mee wilt helpen met de ontwikkeling van de IceTopia Bot, ga naar de [IceTopia Project website](https://sites.google.com/view/icetopia-project/home)."
-      },
-      {
-        name: "GitHub",
-        value: "U zult het openbare script [hier](https://github.com/AdrianKusmierek/icebot2/) kunnen vinden."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Adrian"
-    }
-  }
     
     if (command === 'gm') {
 	    message.channel.send('Good Morning **' + message.member.user.tag + '** !!https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jpg');
